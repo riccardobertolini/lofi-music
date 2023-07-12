651cbb61601eec9bc18d375ea4c42f60e0207e43
riccardobertolini <info@riccardobertolini.net>
2023-07-12 16:37:45 +0200

    feat: adjust shuffle icon

--- components/Shuffler.tsx
+++ components/Shuffler.tsx
@@ -3,17 +3,18 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   .shuffler {
-    height: 50px;
-    width: 50px;
+    height: 36px;
+    width: 36px;
     cursor: pointer;
     color: #fff;
     position: absolute;
-    top: 20px;
-    right: 80px;
+    top: 6px;
+    right: 90px;
     content: "";
     background-size: contain;
     background-image: url('/img/shuffle.png');
     display: block;
+    background-repeat: no-repeat;
   }
 
   @media (max-width: 768px) {
