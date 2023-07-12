8d29a3c2ec0cbb0cce33376b5880f61daf16bc9e
Asad Makhdoom <asad.makhdoom@starzplayarabia.com>
2023-07-12 19:37:09 +0500

    minor styling fix for shuffler icons placement

--- components/Shuffler.tsx
+++ components/Shuffler.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
 const Container = styled.div`
   .shuffler {
@@ -7,10 +7,8 @@ const Container = styled.div`
     width: 50px;
     cursor: pointer;
     color: #fff;
-    position: absolute;
-    top: 20px;
-    right: 80px;
-    content: "";
+
+    content: '';
     background-size: contain;
     background-image: url('/img/shuffle.png');
     display: block;
@@ -24,25 +22,36 @@ const Container = styled.div`
       right: 80px;
     }
   }
-`;
+`
 
-const Shuffler = ({ setRandomTracks, totalTracks }: { setRandomTracks: (value: number[]) => void, totalTracks: number }) => {
-    const getMultipleRandomTracks = (arr: number[], num: number) => {
-        const newArr = [...arr];
-        for (let i = newArr.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
-        }
-        return newArr.slice(0, num);
+const Shuffler = ({
+  setRandomTracks,
+  totalTracks,
+}: {
+  setRandomTracks: (value: number[]) => void
+  totalTracks: number
+}) => {
+  const getMultipleRandomTracks = (arr: number[], num: number) => {
+    const newArr = [...arr]
+    for (let i = newArr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[newArr[i], newArr[j]] = [newArr[j], newArr[i]]
     }
+    return newArr.slice(0, num)
+  }
 
-    return (
+  return (
     <Container>
-        <div className="shuffler" onClick={
-          () => setRandomTracks(getMultipleRandomTracks(Array.from(Array(totalTracks).keys()), 3))
-        } />
-    </Container >
-  );
-};
+      <div
+        className="shuffler"
+        onClick={() =>
+          setRandomTracks(
+            getMultipleRandomTracks(Array.from(Array(totalTracks).keys()), 3),
+          )
+        }
+      />
+    </Container>
+  )
+}
 
-export default Shuffler;
+export default Shuffler
