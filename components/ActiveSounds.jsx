33d30da5a9b0e98d532fbd20a1b1256906377751
miikisalminen <miika.salminen11@outlook.com>
2023-07-12 15:05:26 +0300

    Next.js refactor, cleaned up the styling

---
+++ components/ActiveSounds.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Subheading } from './App.style'
+
+const ActiveSounds = ({ activeSounds, stopAll }) => {
+  if (activeSounds)
+    return (
+      <Subheading style={{ transition: 'opacity 0.3s ease' }}>
+        Sounds active: {activeSounds} <a onClick={stopAll}>Stop all</a>
+      </Subheading>
+    )
+  else
+    return (
+      <Subheading style={{ opacity: '0', transition: 'opacity 0.3s ease' }}>
+        Sounds active: {activeSounds} <a onClick={stopAll}>Stop all</a>
+      </Subheading>
+    )
+}
+
+export default ActiveSounds
--- components/tilePlayer.tsx
+++ components/tilePlayer.tsx
@@ -1,81 +1,101 @@
-import React, { useRef, useState } from 'react';
-import ReactPlayer from 'react-player';
-import { AudioPlayer, ImageContainer, Controls, SliderContainer, StyledSlider } from './tilePlayer.style';
+import React, { useRef, useState, useEffect } from 'react'
+import ReactPlayer from 'react-player'
+import {
+  AudioPlayer,
+  ImageContainer,
+  Controls,
+  SliderContainer,
+  StyledSlider,
+} from './tilePlayer.style'
 
 interface TilePlayerProps {
-  src: string;
-  imageSrc: string;
+  src: string
+  imageSrc: string
+  incrementActiveSounds: (boolean) => void
+  stopAllTrigger: number
 }
 
 interface ProgressState {
-  played: number;
-  playedSeconds: number;
-  loaded: number;
-  loadedSeconds: number;
+  played: number
+  playedSeconds: number
+  loaded: number
+  loadedSeconds: number
 }
 
-export const TilePlayer = ({ src, imageSrc }: TilePlayerProps) => {
-  const [playing, setPlaying] = useState(false);
-  const [volume, setVolume] = useState(1);
-  const [progress, setProgress] = useState(0);
+export const TilePlayer = ({
+  src,
+  imageSrc,
+  incrementActiveSounds,
+  stopAllTrigger,
+}: TilePlayerProps) => {
+  const [playing, setPlaying] = useState(false)
+  const [volume, setVolume] = useState(1)
+  const [progress, setProgress] = useState(0)
 
-  const playerRef = useRef<ReactPlayer>(null);
+  const playerRef = useRef<ReactPlayer>(null)
+
+  useEffect(() => {
+    setPlaying(false)
+  }, [stopAllTrigger])
 
   const togglePlay = () => {
-    setPlaying((prevPlaying) => !prevPlaying);
-  };
+    setPlaying((prevPlaying) => !prevPlaying)
+    incrementActiveSounds(!playing)
+  }
 
   const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newVolume = parseFloat(e.target.value);
-    setVolume(newVolume);
-  };
+    const newVolume = parseFloat(e.target.value)
+    setVolume(newVolume)
+  }
 
   const handleProgress = (state: ProgressState) => {
-    const { played } = state;
-    setProgress(played);
-  };
+    const { played } = state
+    setProgress(played)
+  }
 
   const handleSeek = (value: number) => {
     if (playerRef.current) {
-      playerRef.current.seekTo(value);
+      playerRef.current.seekTo(value)
     }
-  };
+  }
 
   return (
-      <AudioPlayer>
-        <ImageContainer status={playing} onClick={togglePlay}>
-          <img src={imageSrc} alt="" />
-          <ReactPlayer
-              ref={playerRef}
-              url={src}
-              playing={playing}
-              loop
-              volume={volume}
-              width={0}
-              height={0}
-              onProgress={handleProgress}
-          />
-        </ImageContainer>
-        <SliderContainer>
-          <StyledSlider
-              min={0}
-              max={1}
-              step={0.01}
-              value={progress}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSeek(parseFloat(e.target.value))}
-          />
-        </SliderContainer>
-        <Controls>
-          <span>🔊</span>
-          <input
-              type="range"
-              min={0}
-              max={1}
-              step={0.01}
-              value={volume}
-              onChange={handleVolumeChange}
-          />
-        </Controls>
-      </AudioPlayer>
-  );
-};
+    <AudioPlayer>
+      <ImageContainer status={playing} onClick={togglePlay}>
+        <img src={imageSrc} alt="" />
+        <ReactPlayer
+          ref={playerRef}
+          url={src}
+          playing={playing}
+          loop
+          volume={volume}
+          width={0}
+          height={0}
+          onProgress={handleProgress}
+        />
+      </ImageContainer>
+      <SliderContainer>
+        <StyledSlider
+          min={0}
+          max={1}
+          step={0.01}
+          value={progress}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            handleSeek(parseFloat(e.target.value))
+          }
+        />
+      </SliderContainer>
+      <Controls>
+        <span>🔊</span>
+        <input
+          type="range"
+          min={0}
+          max={1}
+          step={0.01}
+          value={volume}
+          onChange={handleVolumeChange}
+        />
+      </Controls>
+    </AudioPlayer>
+  )
+}
