9ebda9a7afcc3208774e8d8d832c1054b9598116
Asad Makhdoom <asad.makhdoom@starzplayarabia.com>
2023-07-12 18:49:12 +0500

    added color enums for consistency across the app

---
+++ constants/colors.ts
@@ -0,0 +1,11 @@
+export enum MuiVariants {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+  ACTION = 'action',
+  DISABLED = 'disabled',
+  NEUTRAL = 'white',
+}
+
+export enum ColorCodes {
+  WHITE = '#ffff',
+}
