0420e89a4dddb22d9437474d3f1cb4a8bc295c03
Riccardo Bertolini <info@riccardobertolini.net>
2023-07-21 17:55:04 +0200

    refactor: separate style from component

--- components/Shuffler.tsx
+++ components/Shuffler.tsx
@@ -1,31 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  .shuffler {
-    height: 36px;
-    width: 36px;
-    cursor: pointer;
-    color: #fff;
-    position: absolute;
-    top: 6px;
-    right: 90px;
-    content: "";
-    background-size: contain;
-    background-image: url('/img/shuffle.png');
-    display: block;
-    background-repeat: no-repeat;
-  }
-
-  @media (max-width: 768px) {
-    .shuffler {
-      height: 35px;
-      width: 35px;
-      top: 10px;
-      right: 80px;
-    }
-  }
-`;
+import {Container} from './Shuffler.style'
 
 const Shuffler = ({ setRandomTracks, totalTracks }: { setRandomTracks: (value: number[]) => void, totalTracks: number }) => {
     const getMultipleRandomTracks = (arr: number[], num: number) => {
