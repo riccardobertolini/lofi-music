9c10bfcadf2f5331cf4a1d99f1379106bc69c9e7
Jonas Lorincz <jblorincz@sesisolutions.com>
2023-07-23 22:09:56 -0400

    Adjust styling to improve display of settings modal

--- components/SettingModal.style.tsx
+++ components/SettingModal.style.tsx
@@ -18,7 +18,8 @@ export const ModalWrapper = styled.div`
 export const ModalContent = styled.div`
   position: relative;
   width: 40rem;
-  height: 10rem;
+  max-width: 90vw;
+  min-height: 10rem;
   background-color: #fff;
   padding: 20px;
   display: flex;
@@ -59,7 +60,6 @@ export const ColorPickerWrapper = styled.div`
   display: flex;
   justify-content: space-around;
   flex-wrap: wrap;
-  width: 100%;
   padding: 20px;
 
   div {
