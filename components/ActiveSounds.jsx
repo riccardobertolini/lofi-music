cb4f49c1d046ee34b153cadb4aa693c64e0bd601
Jonas Lorincz <jblorincz@sesisolutions.com>
2023-07-25 13:36:07 -0400

    improve style and names

--- components/ActiveSounds.jsx
+++ components/ActiveSounds.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 import { Subheading } from './App.style'
 
 const ActiveSounds = ({ timer, minutes, activeSounds, stopAll }) => {
-  let opacityCount = activeSounds ? 1 : 0;
   
-  let cursor = activeSounds ? 'pointer' : 'default';
+  let opacityValue = activeSounds ? 1 : 0;
+
+  let cursorStyle = activeSounds ? 'pointer' : 'default';
 
     return (
       <>
-        <Subheading style={{ transition: 'opacity 0.3s ease', opacity: opacityCount, cursor: cursor  }}>
-          <p style={{ color: '#ff1361',
-
-}}>
+        <Subheading style={{ transition: 'opacity 0.3s ease', opacity: opacityValue, cursor: cursorStyle  }}>
+          <p style={{ color: '#ff1361'}}>
             {minutes < 10 ? `0${minutes}` : minutes}:
             {timer < 10 ? `0${timer}` : timer}
           </p>
