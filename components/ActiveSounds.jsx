05910940848a5df53ad96f71a45d7b4af4890712
Jonas Lorincz <jblorincz@sesisolutions.com>
2023-07-25 13:32:29 -0400

    Fix content shift when playing a sound

--- components/ActiveSounds.jsx
+++ components/ActiveSounds.jsx
@@ -2,29 +2,22 @@ import React from 'react'
 import { Subheading } from './App.style'
 
 const ActiveSounds = ({ timer, minutes, activeSounds, stopAll }) => {
-  if (activeSounds)
+  let opacityCount = activeSounds ? 1 : 0;
+  
+  let cursor = activeSounds ? 'pointer' : 'default';
+
     return (
       <>
-        <Subheading style={{ transition: 'opacity 0.3s ease' }}>
-          <p style={{ color: '#ff1361' }}>
+        <Subheading style={{ transition: 'opacity 0.3s ease', opacity: opacityCount, cursor: cursor  }}>
+          <p style={{ color: '#ff1361',
+
+}}>
             {minutes < 10 ? `0${minutes}` : minutes}:
             {timer < 10 ? `0${timer}` : timer}
           </p>
           Sounds active: {activeSounds} <br /> <a onClick={stopAll}>Stop all</a>
         </Subheading>
       </>
-    )
-  else
-    return (
-      <Subheading
-        style={{
-          opacity: '0',
-          transition: 'opacity 0.3s ease',
-          cursor: 'default',
-        }}
-      >
-        Sounds active: {activeSounds} <br /> <a onClick={stopAll}>Stop all</a>
-      </Subheading>
     )
 }
 
