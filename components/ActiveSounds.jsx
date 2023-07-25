1f3ecbab66580af22962b4cd59941c0842fe57f2
Jonas Lorincz <jblorincz@sesisolutions.com>
2023-07-25 13:49:25 -0400

    Make link not go to pointer when invisible

--- components/ActiveSounds.jsx
+++ components/ActiveSounds.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Subheading } from './App.style'
 
 const ActiveSounds = ({ timer, minutes, activeSounds, stopAll }) => {
-  
+
   let opacityValue = activeSounds ? 1 : 0;
 
   let cursorStyle = activeSounds ? 'pointer' : 'default';
 
+  let linkStyle = activeSounds ? {} : {pointerEvents: 'none', userSelect: 'false' };
+  
     return (
       <>
         <Subheading style={{ transition: 'opacity 0.3s ease', opacity: opacityValue, cursor: cursorStyle  }}>
@@ -14,7 +16,7 @@ const ActiveSounds = ({ timer, minutes, activeSounds, stopAll }) => {
             {minutes < 10 ? `0${minutes}` : minutes}:
             {timer < 10 ? `0${timer}` : timer}
           </p>
-          Sounds active: {activeSounds} <br /> <a onClick={stopAll}>Stop all</a>
+          Sounds active: {activeSounds} <br /> <a style={linkStyle} onClick={stopAll}>Stop all</a>
         </Subheading>
       </>
     )
